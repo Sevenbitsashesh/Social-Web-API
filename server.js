3e391308df70323a794e8828c5e21ef59e30eef5
Sevenbitsashesh <parmar.ashesh@tristonsoft.com>
2019-02-01 14:27:13 +0530

    SWA2_Init_02_01_2019

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const express = require('express');
 const app = express();
-app.get("/", (req, res) => {
-    res.send({ hello: "world" });
- });
- app.listen(3000);
+const path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
