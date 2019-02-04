c521441fc60508f159a43472da78d89d30205799
Sevenbitsashesh <parmar.ashesh@tristonsoft.com>
2019-02-04 19:27:24 +0530

    SWA2_ExpressMongo_04_02_2019

--- server.js
+++ server.js
@@ -1,56 +1,22 @@
 const express = require('express');
-const firebase = require('firebase');
 const app = express();
 const path = require('path');
-const admin = require('firebase-admin');
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://my-social-a5d83.firebaseio.com"
-  });
-const functions = require('firebase-functions');
-var serviceAccount = require("/home/sevenbits11/Ashesh_Trainee/API/serviceAccountKey.json");
+const cors = require('cors');
 
+app.use(cors({ origin: true }));
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let social_db_url = 'mongodb://social-sevenbits:Seven123@ds221435.mlab.com:21435/social-app-db';
+let mongoDB = process.env.MONGODB_URI || social_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var db = admin.firestore();
-
-
- 
-  
-app.get('/login', function(req, res) {
-    // db.collection('users').get().then(users => {
-        res.sendFile(path.join(__dirname, 'index.html'));    
-    // })
+mongoose.get(d => {
     
-    //  console.log(res);
 });
 
-app.listen(process.env.PORT || 4000, function() {
-    console.log('Your node js server is running');
-});
-// Initialize Firebase
-// TODO: Replace with your project's customized code snippet
-  // Initialize Firebase
-  
-app.get('/users',(req,res) => {    
-    // db.collection('users').get().then(u => {
-    //     res.send(u.size);
-    // })
-    getUsers().then(da => {
-        res.render('val',da);
-    })
-    res.send('data');    
-});
-function getUsers(){
-   
-    return db.collection('users').get().then(d => d.size);
-}
-var config = {
-    apiKey: "AIzaSyBqboO_lJPY4SDJbSGbiWzMwhVUIvsTcck",
-    authDomain: "my-social-a5d83.firebaseapp.com",
-    databaseURL: "https://my-social-a5d83.firebaseio.com",
-    projectId: "my-social-a5d83",
-    storageBucket: "my-social-a5d83.appspot.com",
-    messagingSenderId: "22414347988"
-  };
-  firebase.initializeApp(config);
+
+
