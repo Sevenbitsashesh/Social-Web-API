29fc38ce034594ef77e0b30010b629251644f621
Sevenbitsashesh <parmar.ashesh@tristonsoft.com>
2019-02-18 12:13:22 +0530

    SW_Api_Auth_18_02_2019

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const postService = require('../services/post.service');
-router.post('/:postid',getPostByPId);
-router.post('/:userid',getPostByUId);
+// router.post('/:postid',getPostByPId);
+// router.post('/:userid',getPostByUId);
+module.exports = router;
+// module.exports  = {
+//     getPostByPId,
+//     getPostByUId
+// };
+// function getPostByPId(req, res,next) {
 
-module.exports  = {
-    getPostByPId,
-    getPostByUId
-};
-function getPostByPId(req, res,next) {
+// }
+// function getPostByUId(req, res,next) {
 
-}
+// }
