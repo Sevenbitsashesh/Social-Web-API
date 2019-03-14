33ca940d59724ef23716e56d77de5e10872efbc1
Sevenbitsashesh <parmar.ashesh@tristonsoft.com>
2019-03-14 18:49:58 +0530

    SW_Api_SocialAuth_14_03_2019

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -37,7 +37,7 @@ function register(req, res, next) {
 function getUserInfoById(req, res,next) {
     // console.log(req.body);
     
-   userService.getUserInfo(req.body).then(uinfo =>   uinfo.length !== 0 ? res.json(uinfo[0]) : res.json({message: "Not found"})).catch(err => next(err));    
+  return userService.getUserInfo(req.body).then(uinfo =>   uinfo.length !== 0 ? res.json(uinfo[0]) : res.json({message: "Not found"})).catch(err => next(err));    
 }
 function getUserById(req, res, next) {
     // console.log(req.body.userid);
