3ada1316bc5a0d466ba1ddfe2bc995d05c8f57ad
Sevenbitsashesh <parmar.ashesh@tristonsoft.com>
2019-04-19 12:49:26 +0530

    PT_Api_MailService_19_04_2019

--- services/mail.service.js
+++ services/mail.service.js
@@ -1,28 +1,39 @@
 // var nodemailer = require('nodemailer');
 // var transport_manager = require('nodemailer-smtp-transport');
-
+const mailconfig = require('../configs/mail.config');
 module.exports = {    
     sendMail
 }
 var mailgun = require("mailgun-js");
-var api_key = '046883e2536d697ef8e8aae134c786b2-3fb021d1-591ca113';
-var DOMAIN = 'sandbox363636f0eae046d98c8a773bd5c5a1ab.mailgun.org';
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
+
+var mailgun = require('mailgun-js')({apiKey: mailconfig.api_key, domain:mailconfig.DOMAIN});
 
 
 
 
 
-function sendMail({email, fname, lname, pass,trainerid}) {
-    var data = {
-        from: 'PT_APP Sandbox ',
+async function sendMail({email, fname, lname, pass,trainerid}) {
+    
+      new Promise((resolve, reject) => {
+        const data = {
+          from: 'PT_APP <parmar.ashesh@tristonsoft.com>',
         to: email,
         subject: 'You are Added as client of '+trainerid,
         text: 'Your email is : '+email+' and password is :'+pass+' '+'Login in to https://pt-fits-life.firebaseapp.com'
-      };
-      mailgun.messages().send(data, function (error, body) {
-        console.log(body);
-      });
+        };
+    
+        mailgun.messages().send(data, (error) => {
+          if (error) {
+            return reject(error);
+          }
+          return resolve();
+        });
+      // mailgun.messages().send(data, function (error, body) {
+      //   console.log(body);
+      // });
+}).catch(error => {
+  console.log('could not send mail'+error);
+})
 }
 // function getMailoptions(email, pass) {
 //     var mailOptions = {
